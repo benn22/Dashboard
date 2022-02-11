abcb5e66e02d8b1db22ce1996972c49ce1551bc0
Ricardo Castro <40843906+benn22@users.noreply.github.com>
2022-02-11 17:06:00 -0500

    Add files via upload

---
+++ code/orders.js
@@ -0,0 +1,62 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Iphone 13',
+        productNumber: '85630',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'ASUS RTX 3090',
+        productNumber: '85632',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Samsung Galaxy S20',
+        productNumber: '85633',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'MacBook Air M1',
+        productNumber: '85634',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Nike Airforce 1',
+        productNumber: '85635',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Adidas Running',
+        productNumber: '85636',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Mi Note 10',
+        productNumber: '85637',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'MSI RTX 3080TI',
+        productNumber: '85638',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Sony wh-1000xm4',
+        productNumber: '85639',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
